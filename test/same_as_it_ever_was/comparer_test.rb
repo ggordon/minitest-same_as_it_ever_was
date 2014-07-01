db01e45e8cd5d5b3c2abea4455e0630a8d4abb25
Gary Gordon <gfgordon@gmail.com>
2014-07-01 06:41:57 -0400

    add test for simple error case

--- test/same_as_it_ever_was/comparer_test.rb
+++ test/same_as_it_ever_was/comparer_test.rb
@@ -13,6 +13,16 @@ module MiniTest
           flunk 'should\'ve compared successfully '
         end
       end
+      it 'should report an error if 2 simple hashes are different' do
+        h1 = { 'a' => 1 }
+        h2 = { 'a' => 2 }
+        begin
+          Comparer.new(self, h1, h2).compare
+          flunk 'compare worked?'
+        rescue MiniTest::Assertion => e
+          assert_match(/Key 'a' did not match/, e.message)
+        end
+      end
     end
   end
 end
