8d4a1582915b379ac0776cecdd97f86416228fc3
Gary Gordon <gfgordon@gmail.com>
2014-07-11 15:45:43 -0400

    add additional test classes

---
+++ test/same_as_it_ever_was/assertion_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+module MiniTest
+  module SameAsItEverWas
+    describe Assertion do
+      it 'should test something' do
+        assert true
+      end
+    end
+  end
+end
---
+++ test/same_as_it_ever_was/result_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+module MiniTest
+  module SameAsItEverWas
+    describe Result do
+      it 'should test something' do
+        assert true
+      end
+    end
+  end
+end
