2c851b459f04c92467543f0d10999db9354e0d22
Gary Gordon <gfgordon@gmail.com>
2014-07-13 15:58:07 -0400

    result tests

--- test/same_as_it_ever_was/result_test.rb
+++ test/same_as_it_ever_was/result_test.rb
@@ -3,8 +3,32 @@ require 'test_helper'
 module MiniTest
   module SameAsItEverWas
     describe Result do
-      it 'should test something' do
-        assert true
+      let (:result) { MiniTest::SameAsItEverWas::Result.new }
+
+      it 'should have mismatches if set' do
+        result.add_mismatch 'one'
+        result.add_mismatch 'two'
+        result.additional.must_equal ''
+        result.mismatches.must_equal 'one,two'
+        result.missing.must_equal ''
+        result.status.must_equal :fail
+      end
+
+      it 'should have additionals if set' do
+        result.add_additional 'one'
+        result.add_additional 'two'
+        result.additional.must_equal 'one,two'
+        result.mismatches.must_equal ''
+        result.status.must_equal :pass
+      end
+
+      it 'should have missing if set' do
+        result.add_missing 'one'
+        result.add_missing 'two'
+        result.additional.must_equal ''
+        result.mismatches.must_equal ''
+        result.missing.must_equal 'one,two'
+        result.status.must_equal :pass
       end
     end
   end
